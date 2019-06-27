5c345371ab602bdbb8e45c062b83c06d404c3086
mekkj98 <keshavkumarjha890@gmail.com>
2019-06-27 20:34:45 +0530

    Changes for scroll in js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@ window.onload = function () {
         var menu = this.nextElementSibling;
         if (menu.style.display == "" || menu.style.display == "none") {
             menu.style.display = "block";
+            menu.classList.add("active-now");
         } else {
             menu.style.display = "none";
         }
@@ -11,7 +12,8 @@ window.onload = function () {
 }
 
 window.onscroll = function () {
-    if (window.scrollY > 100); {
-        document.getElementById("navToggler").nextElementSibling.style.display = "none";
+    if (window.scrollY > 100) {
+        var menu = document.getElementById("navToggler").nextElementSibling;
+        if (menu.className == "active-now") menu.style.display = "none";
     }
 }
